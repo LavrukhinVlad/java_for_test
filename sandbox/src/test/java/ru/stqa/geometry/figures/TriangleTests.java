f8e3e5ceff6b94ee43826b8bb230d0646c6efec8
Vladislav Lavrukhin <va.lavrukhin@severstal.com>
2025-03-04 15:25:22 +0300

    Задание №4. Сравнение объектов

--- sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
+++ sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
@@ -26,4 +26,19 @@ public class TriangleTests {
             // OK
         }
     }
+
+    @Test
+    void testEquality() {
+        var t1 = new Triangle(3.0, 4.0, 5.0);
+        var t2 = new Triangle(3.0, 4.0, 5.0);
+        Assertions.assertEquals(t1, t2);
+    }
+
+    @Test
+    void testEquality2() {
+        var t1 = new Triangle(3.0, 4.0, 5.0);
+        var t2 = new Triangle(4.0, 5.0, 3.0);
+        Assertions.assertEquals(t1, t2);
+    }
+
 }
