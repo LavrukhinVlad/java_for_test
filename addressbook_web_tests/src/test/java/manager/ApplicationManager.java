80e31a1470801666d354e4dc77157d3b4cbfe519
Vladislav Lavrukhin <va.lavrukhin@severstal.com>
2025-03-07 19:30:19 +0300

    Задание №6: Двухуровневоя архитектура тестового набора

--- addressbook_web_tests/src/test/java/manager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/manager/ApplicationManager.java
@@ -4,15 +4,23 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class ApplicationManager {
     protected WebDriver driver;
     private LoginHelper session;
     private GroupHelper groups;
 
-   public void init() {
+   public void init(String browser) {
         if (driver == null) {
-            driver = new ChromeDriver();
+            if("chrome".equals(browser)){
+                driver = new ChromeDriver();
+            } else if ("edge".equals(browser)) {
+                driver = new EdgeDriver();
+            } else {
+                throw new IllegalArgumentException(String.format("Unknow browser %s", browser));
+            }
             Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
             driver.get("http:/localhost/addressbook/");
             session().login("admin", "secret", this);
