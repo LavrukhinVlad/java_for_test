ba5cb768abf1a2ef76b41b62872bd2c6d2e64668
Vlad <vlad1995lavru@gmail.com>
2025-03-25 15:00:52 +0300

    Задание №9. Параметризация тестов. Сокращение параметр

--- addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
@@ -14,48 +14,15 @@ public class ContactCreationTests extends TestBase {
     public static List<ContactData> contactProvider() {
         var result = new ArrayList<ContactData>();
         for (var firstname : List.of("", "firstname")) {
-            for (var middlename : List.of("", "middlename")) {
                 for (var lastname : List.of("", "lastname")) {
-                    for (var nickname : List.of("", "nickname")) {
-                        for (var title : List.of("", "title")) {
-                            for (var company : List.of("", "company")) {
                                 for (var address : List.of("", "address")) {
-                                    for (var home : List.of("", "home")) {
-                                        for (var mobile : List.of("", "mobile")) {
-                                            for (var work : List.of("", "work")) {
-                                                for (var fax : List.of("", "fax")) {
-                                                    for (var email : List.of("", "email")) {
-                                                        for (var email2 : List.of("", "email2")) {
-                                                            for (var email3 : List.of("", "email3")) {
                                                                 result.add(new ContactData()
                                                                         .withFirstname(firstname)
-                                                                        .withMiddlename(middlename)
                                                                         .withLastname(lastname)
-                                                                        .withNickname(nickname)
-                                                                        .withTitle(title)
-                                                                        .withCompany(company)
-                                                                        .withAddress(address)
-                                                                        .withHome(home)
-                                                                        .withMobile(mobile)
-                                                                        .withWork(work)
-                                                                        .withFax(fax)
-                                                                        .withEmail(email)
-                                                                        .withEmail2(email2)
-                                                                        .withEmail3(email3));
+                                                                        .withAddress(address));
                                                             }
                                                         }
                                                     }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
         for (int i = 0; i < 5; i++) {
             result.add(new ContactData()
                     .withFirstname(randomString(i * 10))
