6c7dddc1eeab7883d62afeb57808594dd3354e08
Vlad <vlad1995lavru@gmail.com>
2025-03-28 12:31:43 +0300

    Лекция 5.4

--- addressbook_web_tests/src/test/java/ru/stqa/addressbook/generator/Generator.java
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/generator/Generator.java
@@ -2,36 +2,40 @@ package ru.stqa.addressbook.generator;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import ru.stqa.addressbook.common.CommonFunctions;
 import ru.stqa.addressbook.model.ContactData;
 import ru.stqa.addressbook.model.GroupData;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Generator {
 
-    @Parameter(names={"--type", "-t"})
+    @Parameter(names = {"--type", "-t"})
     String type;
 
-    @Parameter(names={"--output", "-o"})
+    @Parameter(names = {"--output", "-o"})
     String output;
 
-    @Parameter(names={"--format", "-f"})
+    @Parameter(names = {"--format", "-f"})
     String format;
 
-    @Parameter(names={"--count", "-n"})
+    @Parameter(names = {"--count", "-n"})
     int count;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         var generator = new Generator();
         JCommander.newBuilder()
-                        .addObject(generator)
-                                .build()
-                                        .parse(args);
+                .addObject(generator)
+                .build()
+                .parse(args);
         generator.run();
     }
 
-    private void run() {
+    private void run() throws IOException {
         var data = generate();
         save(data);
     }
@@ -65,7 +69,7 @@ public class Generator {
                     .withMiddlename(CommonFunctions.randomString(i * 10))
                     .withLastname(CommonFunctions.randomString(i * 10))
                     .withNickname(CommonFunctions.randomString(i * 10))
-                    .withNickname(CommonFunctions.randomFile("scr/test/resources/images"))
+                   // .withNickname(CommonFunctions.randomFile("scr/test/resources/images"))
                     .withTitle(CommonFunctions.randomString(i * 10))
                     .withCompany(CommonFunctions.randomString(i * 10))
                     .withAddress(CommonFunctions.randomString(i * 10))
@@ -80,7 +84,13 @@ public class Generator {
         return result;
     }
 
-    private void save(Object data) {
-
+    private void save(Object data) throws IOException {
+        if ("json".equals(format)) {
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.enable(SerializationFeature.INDENT_OUTPUT);
+            mapper.writeValue(new File(output), data);
+        } else {
+            throw new IllegalArgumentException("Неизвестный формат данных" + format);
+        }
     }
 }
--- addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/ContactCreationTests.java
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/ContactCreationTests.java
@@ -1,50 +1,40 @@
 package ru.stqa.addressbook.tests;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import ru.stqa.addressbook.common.CommonFunctions;
 import ru.stqa.addressbook.model.ContactData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+import ru.stqa.addressbook.model.GroupData;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 public class ContactCreationTests extends TestBase {
 
-    public static List<ContactData> contactProvider() {
+    public static List<ContactData> contactProvider() throws IOException {
         var result = new ArrayList<ContactData>();
-        for (var firstname : List.of("", "firstname")) {
-            for (var lastname : List.of("", "lastname")) {
-                for (var address : List.of("", "address")) {
-                    for (var photo : List.of("", "photo")) {
-                        result.add(new ContactData()
-                                .withFirstname(firstname)
-                                .withLastname(lastname)
-                                .withAddress(address)
-                                .withPhoto(photo));
-                    }
-                }
-            }
-        }
-        for (int i = 0; i < 5; i++) {
-            result.add(new ContactData()
-                    .withFirstname(CommonFunctions.randomString(i * 10))
-                    .withMiddlename(CommonFunctions.randomString(i * 10))
-                    .withLastname(CommonFunctions.randomString(i * 10))
-                    .withNickname(CommonFunctions.randomString(i * 10))
-                    .withNickname(CommonFunctions.randomFile("scr/test/resources/images"))
-                    .withTitle(CommonFunctions.randomString(i * 10))
-                    .withCompany(CommonFunctions.randomString(i * 10))
-                    .withAddress(CommonFunctions.randomString(i * 10))
-                    .withHome(CommonFunctions.randomString(i * 10))
-                    .withMobile(CommonFunctions.randomString(i * 10))
-                    .withWork(CommonFunctions.randomString(i * 10))
-                    .withFax(CommonFunctions.randomString(i * 10))
-                    .withEmail(CommonFunctions.randomString(i * 10))
-                    .withEmail2(CommonFunctions.randomString(i * 10))
-                    .withEmail3(CommonFunctions.randomString(i * 10)));
-        }
+//        for (var firstname : List.of("", "firstname")) {
+//            for (var lastname : List.of("", "lastname")) {
+//                for (var address : List.of("", "address")) {
+//                    for (var photo : List.of("", "photo")) {
+//                        result.add(new ContactData()
+//                                .withFirstname(firstname)
+//                                .withLastname(lastname)
+//                                .withAddress(address)
+//                                .withPhoto(photo));
+//                    }
+//                }
+//            }
+//        }
+        ObjectMapper mapper = new ObjectMapper();
+        var value = mapper.readValue(new File("contacts.json"), new TypeReference<List<ContactData>>() {});
+        result.addAll(value);
         return result;
     }
 
