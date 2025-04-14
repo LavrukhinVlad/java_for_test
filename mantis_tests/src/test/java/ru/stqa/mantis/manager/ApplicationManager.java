aae78b54fda33f18ce8c053e98da9a075310f38e
Vlad <vlad1995lavru@gmail.com>
2025-04-14 14:49:49 +0300

    Задание №17. Новый проект

---
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
@@ -0,0 +1,42 @@
+package ru.stqa.mantis.manager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import java.util.Properties;
+
+public class ApplicationManager {
+    private WebDriver driver;
+    private String string;
+    private Properties properties;
+    private SessionHelper sessionHelper;
+
+
+    public void init(String browser, Properties properties) {
+        this.string = browser;
+        this.properties = properties;
+    }
+
+    public WebDriver driver() {
+        if (driver == null) {
+            if ("chrome".equals(string)) {
+                driver = new ChromeDriver();
+            } else if ("edge".equals(string)) {
+                driver = new EdgeDriver();
+            } else {
+                throw new IllegalArgumentException(String.format("Unknown browser %s", string));
+            }
+            Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
+            driver.get(properties.getProperty("web.baseUrl"));
+        }
+        return driver;
+    }
+
+    public SessionHelper session() {
+        if (sessionHelper == null) {
+            sessionHelper = new SessionHelper(this);
+        }
+        return sessionHelper;
+    }
+}
